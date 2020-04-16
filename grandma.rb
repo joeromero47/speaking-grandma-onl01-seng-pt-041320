3fbb43b0e7b8f350e82443429bf2cc4e41c47642
Joseph Romero <joeromero47@gmail.com>
2020-04-16 23:05:50 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -12,12 +12,7 @@
 # However if you say 'I LOVE YOU GRANDMA!', she should respond with
 # 'I LOVE YOU TOO PUMPKIN!'
 
-def phrase (grandma)
-  if capitals
-    puts "NO, NOT SINCE 1938!"
-  if "I LOVE YOU GRANDMA!"   
-    puts "I LOVE YOU TOO PUMPKIN!"
-  elsif puts "HUH?! SPEAK UP, SONNY!"
-end
+def phrase("grandma")
+  
 
   
