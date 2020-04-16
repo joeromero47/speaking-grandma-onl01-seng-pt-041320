fd19e004407acc57d460b7c4ce7ffcd6164e12b8
Joseph Romero <joeromero47@gmail.com>
2020-04-16 23:13:13 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -15,7 +15,17 @@
 def speak_with_grandma("phrase")
   if phrase="I LOVE YOU GRANDMA!" 
     return "I LOVE YOU TOO PUMPKIN!"
-  elseif phrase.downcase
+  elsif phrase==phrase.downcase
+    return "HUH?! SPEAK UP, SONNY!"
+  elsif phrase==phrase.uppercase
+    return "NO, NOT SINCE 1938!"
+  end
+  
+speak_with_grandma("hi")
+
+speak_with_grandma("HI!")
+
+speak_with_grandma("I LOVE YOU GRANDMA!")
     
   
   
