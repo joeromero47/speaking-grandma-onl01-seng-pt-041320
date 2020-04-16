cc83a40f731bcd1ded7f665967d73b1d303433c9
Joseph Romero <joeromero47@gmail.com>
2020-04-16 23:27:16 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -15,11 +15,9 @@
 def speak_to_grandma(phrase)
   if phrase == "I LOVE YOU GRANDMA!" 
     return "I LOVE YOU TOO PUMPKIN!"
-  elsif phrase == "phrase".downcase
-    return "HUH?! SPEAK UP, SONNY!"
   elsif phrase == "phrase".uppercase
     return "NO, NOT SINCE 1938!"
-  elsif phrase == "phrase".capitalize
+  else phrase == "phrase".capitalize
     return "HUH?! SPEAK UP, SONNY!"
   end
 end
